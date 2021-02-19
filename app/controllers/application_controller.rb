c228fb5f543d2a63df6802adcf21f10302e4bf73
jones-py <jones03chimfwembe@gmail.com>
2021-02-19 21:45:13 +0900

    registrations controller created to add extrafields to Users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:gender, :DOB])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name,:gender, :DOB])
-  end
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  #
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: %i[:name,:gender, :DOB])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: %i[:name,:gender, :DOB])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :courses
   resources :course_applications
-  devise_for :users
-
+  devise_for :users, controllers: { registrations: 'registrations'}
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
