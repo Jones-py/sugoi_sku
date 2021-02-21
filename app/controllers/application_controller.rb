4e7c8e34e5a1d81f9d9d5b09fff37c0445482026
jones-py <jones03chimfwembe@gmail.com>
2021-02-21 14:29:21 +0900

    pictures folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
+  
+
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(resource)
-    root_path
-  end
+
   # before_action :configure_permitted_parameters, if: :devise_controller?
-  #
+
   # def configure_permitted_parameters
   #   devise_parameter_sanitizer.permit(:sign_up, keys: %i[:name,:gender, :DOB])
   #   devise_parameter_sanitizer.permit(:account_update, keys: %i[:name,:gender, :DOB])
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
  load_and_authorize_resource
-private
-
+ protected
+ def after_inactive_sign_up_path_for(resource)
+   home_index_path# Or :prefix_to_your_route
+ end
+ private
   def sign_up_params
-    params.require(:user).permit(:first_name,:last_name,:gender, :DOB,:email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :gender, :DOB,:email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name,:last_name,:gender, :DOB, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :last_name, :gender, :DOB, :email, :password, :password_confirmation, :current_password)
   end
 
 end
