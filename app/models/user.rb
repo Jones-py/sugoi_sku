06b940f679d6efe7fdbc66f2f4f560ac4e6b6897
jones-py <jones03chimfwembe@gmail.com>
2021-03-05 18:09:45 +0900

    disable confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :aplications
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,:confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
