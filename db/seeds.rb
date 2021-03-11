80a78c2c881e3b9b83ae6d6f70bce099df67fbe5
jones-py <jones03chimfwembe@gmail.com>
2021-03-11 23:57:57 +0900

    rake seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,10 @@
 
 30.times do |t|
-  name = Faker::Games::Pokemon.name
-  email = Faker::Internet.email
-  password = "password"
-  user = User.create! :first_name => "jones#{t}",
+  user = User.create! :first_name => "john#{t}",
                       :last_name => "chansa #{t}",
                       :mobile_number => "096615678#{t}",
                       :date_of_birth => '2/5/2002',
-                      :email => "jones#{t}@gmail.com",
+                      :email => "john#{t}@gmail.com",
                       :password  => "jones94#{t}",
                       :password_confirmation => "jones94#{t}",
                       :superadmin_role => 'true'
