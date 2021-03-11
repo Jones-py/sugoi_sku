193a1499fa081bad6d601400a8bd7b80e8f08001
jones-py <jones03chimfwembe@gmail.com>
2021-03-11 23:43:36 +0900

    comment and users rspec

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
 
-30.times do |n|
+30.times do |t|
   name = Faker::Games::Pokemon.name
   email = Faker::Internet.email
   password = "password"
-  user = User.create! :first_name => 'jones',
-                      :last_name => 'chansa',
-                      :mobile_number => '096615678',
+  user = User.create! :first_name => "jones#{t}",
+                      :last_name => "chansa #{t}",
+                      :mobile_number => "096615678#{t}",
                       :date_of_birth => '2/5/2002',
-                      :email => 'jones@gmail.com',
-                      :password  => 'jones94',
-                      :password_confirmation => 'jones94',
+                      :email => "jones#{t}@gmail.com",
+                      :password  => "jones94#{t}",
+                      :password_confirmation => "jones94#{t}",
                       :superadmin_role => 'true'
+
+
 end
